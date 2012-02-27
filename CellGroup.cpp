9614b5dbd9f0ee69dd29dd5e529c5cd020b469ac
dlopes <dlopes@nd.edu>
2012-02-27 03:57:35 +0000

    Updating... lots of couts for testing purposes.

--- CellGroup.cpp
+++ CellGroup.cpp
@@ -1,17 +1,75 @@
 //CellGroup.cpp
 #include "CellGroup.h"
 
-CellGroup::CellGroup () {
-  standardOrderCounter = 0;
+#include <iostream> // FOR TESTING                                                                                                                                                                                                  
+
+CellGroup::CellGroup (vector<Cell*> c) {
+  std::cout << "Size of cell vector: " << c.size() << endl;
+  SMOCounter = 0;
+  cells = c;
+  controlGroup = 0;
+}
+
+CellGroup::CellGroup (Cell* c) {
+  SMOCounter = 0;
+  cells.push_back( c );
 }
 
 CellGroup::~CellGroup () {}
 
-CellGroup::void drawMe () {}
+void CellGroup::drawMe () {
 
-//The default neighbor handler -- do nothing
-CellGroup::void handleNeighbors (vector<CellGroup*> neighbors) {}
+  for (vector<Cell*>::iterator i = cells.begin(); i != cells.end(); ++i) {
+    (*i)->draw();
+  }
 
-CellGroup::void move () {}
+}
 
-CellGroup::void queueStandardOrders () {}
+// The default neighbor handler -- do nothing                                                                                                                                                                                       
+void CellGroup::handleNeighbors (vector<CellGroup*> neighbors) {}
+
+void CellGroup::move (Direction dir) {
+  for (int i = 0; i < cells.size(); ++i) {
+    cells.at(i)->move( dir );
+  }
+}
+
+void CellGroup::queueStandardMovementOrders (int cycles) {
+
+}
+
+void CellGroup::issueMovementOrder (Direction dir) {
+
+  movementQueue.push_back(dir);
+
+}
+
+// MOVEMENT OCCURS ON THE UPCYCLE                                                                                                                                                                                     
+void CellGroup::upCycle () {
+
+  if ( movementQueue.empty() ) return; // Do not move if there are no movement orders                                                                                                                                               
+  for (int i = 0; i < cells.size(); ++i) {
+    cells.at( i )->move( movementQueue.front() );
+  }
+
+  movementQueue.pop_front();
+}
+
+void CellGroup::downCycle () {
+
+}
+
+
+vector<Location> CellGroup::getLocations () {
+  vector<Location> locations;
+  for (int i = 0; i < cells.size(); i++) {
+    locations.push_back( cells.at(i)->getGridLocation() );
+    cout << "Returning grid location " << i << endl;
+  }
+
+  return locations;
+}
+
+char CellGroup::getImage () {
+  return 'O';
+}
