cb1e9257d32c74f1b58f8d529b5dedee88abb38d
Mark Riehm <mark@mark-Studio-1558.(none)>
2012-04-10 15:49:34 -0400

    Revert "Added InfoBox.h"
    This reverts commit 0ca0cd9aa16dcdd11ddb5c23399ff4822332891c.

--- main.cpp
+++ main.cpp
@@ -38,8 +38,8 @@ int main (void) {
 
   ImageCache::LoadFromDirectory( "./images/" ); // Initialize image cache
 //  Level level = LevelParser::parse (LEVELFILE, window);
-  Level& level( LevelParser::Parse (LEVELFILE, window) );
-  InfoBox infoBox(LEVELFILE, level, window);
+  Level* level = new Level( LevelParser::Parse ("levels/level_T1.bit", window) );
+  InfoBox infoBox(LEVELFILE, *level, window);
 
   // INITIALIZE INPUT VARIABLES
   Location   Lorder; // Input interpreted as location
@@ -67,55 +67,55 @@ int main (void) {
 	case sf::Key::Left:
 	  Dorder.x = -1;
 	  Dorder.y = 0;
-	  level.handleInput(Dorder);
+	  level->handleInput(Dorder);
 	  break;
 	case sf::Key::Right:
 	  Dorder.x = 1;
 	  Dorder.y = 0;
-	  level.handleInput(Dorder);
+	  level->handleInput(Dorder);
 	  break;
 	case sf::Key::Up:
 	  Dorder.x = 0;
 	  Dorder.y = -1;
-	  level.handleInput(Dorder);
+	  level->handleInput(Dorder);
 	  break;
 	case sf::Key::Down:
 	  Dorder.x = 0;
 	  Dorder.y = 1;
-	  level.handleInput(Dorder);
+	  level->handleInput(Dorder);
 	  break;
 	case sf::Key::S:
 	  Dorder.x = 0;
 	  Dorder.y = 0;
-	  level.handleInput(Dorder);
+	  level->handleInput(Dorder);
 	  break;
 	case sf::Key::Back:
-	  level.handleInput(sf::Key::Back);
+	  level->handleInput(sf::Key::Back);
 	  break;
 	case sf::Key::Space:
-	  level.handleInput(sf::Key::Space);
+	  level->handleInput(sf::Key::Space);
 	  break;
         }
 
       if(Event.Type == sf::Event::MouseButtonReleased)
         if(Event.MouseButton.Button == sf::Mouse::Left) //left mouse click
-          level.prepareInput(Event.MouseButton.X, Event.MouseButton.Y, 0); //the 0 flag indicates a left mouse click
+          level->prepareInput(Event.MouseButton.X, Event.MouseButton.Y, 0); //the 0 flag indicates a left mouse click
 	else if (Event.MouseButton.Button == sf::Mouse::Right) { //right mouse click
-	  level.prepareInput(Event.MouseButton.X, Event.MouseButton.Y, 1); //the 1 flag indicates a right mouse click
+	  level->prepareInput(Event.MouseButton.X, Event.MouseButton.Y, 1); //the 1 flag indicates a right mouse click
 	}
     }
 
-    if( level.done() ) {
-      cout << "Level is done and the new level should be " << level.nextLevel () << endl;
-      string newLevel = level.nextLevel();
-      level.destroy();
-      level = Level( LevelParser::Parse( newLevel.c_str(), window) );
-//      level = nextLevel;
+    if( level->done() ) {
+      cout << "Level is done and the new level should be " << level->nextLevel () << endl;
+      string newLevel = level->nextLevel();
+      Level* nextLevel = new Level( LevelParser::Parse( newLevel.c_str(), window) );
+      level->destroy();
+      level = nextLevel;
     }
 
     window.Clear();
 
-    level.draw(); //Draw level on window
+    level->draw(); //Draw level on window
     infoBox.draw();
     window.Display(); //Display window
   }
